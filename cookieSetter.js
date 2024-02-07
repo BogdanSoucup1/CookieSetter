b9252b2525914808579f03ebecd47749b72e091d
Soucup Bogdan <bogdan.socup@2performant.com>
2024-02-07 11:57:41 +0200

    WIP

--- cookieSetter.js
+++ cookieSetter.js
@@ -2,7 +2,7 @@ function setCookie(name, value, daysToExpire) {
   var expirationDate = new Date();
   expirationDate.setDate(expirationDate.getDate() + daysToExpire);
 
-  var cookieString = name + '=' + encodeURIComponent(value) + '; expires=' + expirationDate.toUTCString() + '; samesite=none; domain=.github.io;';
+  var cookieString = name + '=' + encodeURIComponent(value) + '; expires=' + expirationDate.toUTCString() + '; samesite=none;';
 
   document.cookie = cookieString;
 }
